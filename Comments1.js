493d02ad17eda572f3af25849320a45070943e4d
SugandhaVS95 <118116717+SugandhaVS95@users.noreply.github.com>
2023-07-18 19:55:16 +0000

    Copilot third commit

---
+++ Comments1.js
@@ -0,0 +1,29 @@
+// create web server
+// const server = http.createServer((req, res) => {
+//   // send HTTP header
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+// 
+//   // send response body as "Hello World"
+//   res.end('Hello World!\n');
+// });
+// 
+// // listen to port 3000
+// server.listen(3000, 'localhost', () => {
+//   // console will print the message
+//   console.log('Server running at http://localhost:3000/');
+// });
+// create web server
+const server = http.createServer((req, res) => {
+  // send HTTP header
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  // send response body as "Hello World"
+  res.end('Hello World!\n');
+});
+
+// listen to port 3000
+server.listen(3000, 'localhost', () => {
+  // console will print the message
+  console.log('Server running at http://localhost:3000/');
+});
+/
