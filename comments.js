718ef593e623130717f2e23850647f3581777592
SugandhaVS95 <118116717+SugandhaVS95@users.noreply.github.com>
2023-07-19 01:16:49 +0530

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,21 +1,16 @@
-// create web server
-// run this file using: node comments.js
-// then go to browser and type: localhost:3000
-// you should see the message Hello World! displayed in the browser window
-
-var http = require('http');
+const http = require('http');
 
 // create web server
-http.createServer(function (req, res) {
-	// send HTTP header
-	// HTTP status: 200 : OK
-	// content type: text/plain
-	res.writeHead(200, {'Content-Type': 'text/plain'});
+const server = http.createServer((req, res) => {
+  // send HTTP header
+  res.writeHead(200, {'Content-Type': 'text/plain'});
 
-	// send response body as "Hello World"
-	res.end('Hello World!\n');
-}).listen(3000);
+  // send response body as "Hello World"
+  res.end('Hello World!\n');
+});
 
-// console will print the message
-console.log('Server running at http://localhost:3000/');
+// listen to port 3000
+server.listen(3000, 'localhost', () => {
+  // console will print the message
+  console.log('Server running at http://localhost:3000/');
 });
